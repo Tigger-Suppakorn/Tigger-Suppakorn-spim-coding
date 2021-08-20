2c44f7a7553e24ca47ebeaa265e8860e2acef792
Suppakorn Chartjirakarn <72692256+Tigger-Suppakorn@users.noreply.github.com>
2021-08-20 17:59:30 +0700

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int W ;
+int main(void) {
+  printf("enter word =");
+  scanf("%d ", &W);
+  if ("%d = gta",&W){
+  printf("False");
+  }else if ("%d = trgbgrt", &W){
+   printf("true");
+  }
+  
+  
+}
